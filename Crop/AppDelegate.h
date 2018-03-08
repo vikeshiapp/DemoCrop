0aee27743b0c627439dc8fecfc4b620a45ee10d0
iApp <iapp@iapps-imac-89.local>
2018-03-08 18:50:59 +0530

    Initial Commit

---
+++ Crop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crop
+//
+//  Created by iApp on 08/03/18.
+//  Copyright © 2018 iApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Crop
+//
+//  Created by iApp on 08/03/18.
+//  Copyright © 2018 iApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
